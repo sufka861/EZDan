9fbf78bf98e07638bd85303265d9e392af88014a
Suf <sufkarmon2@gmail.com>
2022-04-28 00:51:20 +0300

    First commit

---
+++ js/frm-scripts.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+    const addressSearch = document.getElementById("address-inpt-field");
+    const addressBtn = document.getElementById("address-search-btn");
+    const destBtn = document.getElementById("save-dest-btn");
+
+    addressSearch.onkeydown = () => {
+        addressBtn.disabled = false;
+    }
+
+    addressBtn.onclick = () => {
+        document.getElementById("map-pic").src = "/images/grandmas_home_map.png";
+        destBtn.disabled = false;
+    }
+
+    $('#my-modal').on('shown.bs.modal', function () {
+        $('#modal-link').on('click', function (e) {
+            window.location.replace("noas-destination-2.html");
+        })
+    })
+    
+}
+
